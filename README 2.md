eeb95e08b054f8d5a153e7defafb0e5df7e0a590
SteveKingFord <332312568@qq.com>
2021-04-17 21:39:06 +0800

    chore: 集成commitlint提交规范

--- README 2.md
+++
@@ -1,7 +0,0 @@
-<!--
- * @Author: kingford
- * @Date: 2021-04-16 09:39:51
- * @LastEditTime: 2021-04-16 09:40:11
--->
-
-version 1.0.0 init project
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,1 @@
-/*
- * @Author: kingford
- * @Date: 2021-04-17 20:08:02
- * @LastEditTime: 2021-04-17 20:08:14
- */
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-};
+module.exports = {extends: ['@commitlint/config-conventional']}
