1183fb0d95cb5d38cb57d49efe9b743154e1b30d
SteveKingFord <332312568@qq.com>
2021-04-19 20:06:39 +0800

    feat: 按需加载

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 /*
  * @Author: kingford
  * @Date: 2021-04-16 09:54:56
- * @LastEditTime: 2021-04-17 20:51:32
+ * @LastEditTime: 2021-04-19 20:03:09
  */
+import { formatDate } from './date';
+import { debounce } from './debouce';
+export { debounce, formatDate };
 
-import * as date from './date';
-import * as debouce from './debouce';
+// import * as date from './date';
+// import * as debouce from './debouce';
 
-export default {
-  ...debouce,
-  ...date
-};
+// export default {
+//   ...debouce,
+//   ...date
+// };
