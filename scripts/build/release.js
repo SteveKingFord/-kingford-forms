906905ec51b88b7e69c4b002c36f6f69f83d994f
kingford <stevekingford@gmail.com>
2021-08-05 12:01:35 +0800

    chore: release文件变更

--- scripts/build/release.js
+++ scripts/build/release.js
@@ -1,14 +1,13 @@
-/*
- * @Author: kingford
- * @Date: 2021-08-04 17:58:03
- * @LastEditTime: 2021-08-05 09:26:19
- */
 const args = require('minimist')(process.argv.slice(2))
 const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
 const semver = require('semver')
-const currentVersion = require('../package.json').version
+
+const PKG_URL = '../../package'
+const PKG_FULL_URL = PKG_URL + '.json'
+
+const currentVersion = require(`${PKG_URL}.json`).version
 const { prompt } = require('enquirer')
 const execa = require('execa')
 
@@ -19,7 +18,7 @@ const isDryRun = args.dry
 const skipTests = args.skipTests
 const skipBuild = args.skipBuild
 const packages = fs
-  .readdirSync(path.resolve(__dirname, '../packages'))
+  .readdirSync(path.resolve(__dirname, '../../packages'))
   .filter(p => !p.endsWith('.ts') && !p.startsWith('.'))
 
 const skippedPackages = []
@@ -32,13 +31,13 @@ const versionIncrements = [
 ]
 
 const inc = i => semver.inc(currentVersion, i, preId)
-const bin = name => path.resolve(__dirname, '../node_modules/.bin/' + name)
+const bin = name => path.resolve(__dirname, '../../node_modules/.bin/' + name)
 const run = (bin, args, opts = {}) =>
   execa(bin, args, { stdio: 'inherit', ...opts })
 const dryRun = (bin, args, opts = {}) =>
   console.log(chalk.blue(`[dryrun] ${bin} ${args.join(' ')}`), opts)
 const runIfNotDry = isDryRun ? dryRun : run
-const getPkgRoot = pkg => path.resolve(__dirname, '../packages/' + pkg)
+const getPkgRoot = pkg => path.resolve(__dirname, '../../packages/' + pkg)
 const step = msg => console.log(chalk.cyan(msg))
 
 async function main() {
@@ -167,7 +166,7 @@ function updateDeps(pkg, depType, version) {
   Object.keys(deps).forEach(dep => {
     if (
       dep === 'jin' ||
-      (dep.startsWith('@jin') && packages.includes(dep.replace(/^@jin\//, '')))
+      (dep.startsWith('@kf') && packages.includes(dep.replace(/^@kf\//, '')))
     ) {
       console.log(
         chalk.yellow(`${pkg.name} -> ${depType} -> ${dep}@${version}`)
@@ -188,6 +187,8 @@ async function publishPackage(pkgName, version, runIfNotDry) {
     return
   }
 
+  // For now, all 3.x packages except "vue" can be published as
+  // `latest`, whereas "vue" will be published under the "next" tag.
   let releaseTag = null
   if (args.tag) {
     releaseTag = args.tag
