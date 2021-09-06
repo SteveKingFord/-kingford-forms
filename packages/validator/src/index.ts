4d297c4fea37fbc7d1eee576c331feeb0d691863
kingford <32887862+skingford@users.noreply.github.com>
2021-09-06 08:38:44 +0800

    feat: 身份证验证

--- packages/validator/src/index.ts
+++ packages/validator/src/index.ts
@@ -55,7 +55,8 @@ export function isIdCard(idCard: string): boolean {
   if (!idCard || !isString(idCard)) {
     return false
   }
-  const reg = /(^d{15}$)|(^d{18}$)|(^d{17}(d|X|x)$)/
+  const reg =
+    /(^\d{8}(0\d|10|11|12)([0-2]\d|30|31)\d{3}$)|(^\d{6}(18|19|20)\d{2}(0[1-9]|10|11|12)([0-2]\d|30|31)\d{3}(\d|X|x)$)/
 
   return reg.test(idCard)
 }
